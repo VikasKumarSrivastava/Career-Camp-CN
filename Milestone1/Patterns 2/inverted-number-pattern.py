4e11cf2cda877e43d520c0762b5b3f5b8f74416b
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2023-06-19 20:27:17 +0530

    Create inverted-number-pattern.py

---
+++ Milestone1/Patterns 2/inverted-number-pattern.py
@@ -0,0 +1,15 @@
+# Pattern for N = 4
+# 4444
+# 333
+# 22
+# 1
+
+n = int(input())
+i = 1
+while i <= n:
+    j=1
+    while j<= n-i+1:
+        print(n-i+1,end="")
+        j=j+1
+    print()
+    i = i+1

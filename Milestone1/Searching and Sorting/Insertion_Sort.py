7a5375ca9f8b8466bf5a2b49d55612634debae02
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2023-06-25 01:29:32 +0530

    Update Insertion_Sort.py

--- Milestone1/Searching and Sorting/Insertion_Sort.py
+++ Milestone1/Searching and Sorting/Insertion_Sort.py
@@ -5,3 +5,17 @@
 # 2 13 4 1 3 6 28
 # Sample Output 1:
 # 1 2 3 4 6 13 28
+from sys import stdin
+
+def insertionSort(arr, n) :  
+    #Your code goes here
+    for i in range(1,n):
+        j = i-1
+        temp = arr[i]
+        while j >= 0 and arr[j] > temp:
+            arr[j+1] = arr[j]
+            j = j -1
+        arr[j+1] = temp
+
+
+

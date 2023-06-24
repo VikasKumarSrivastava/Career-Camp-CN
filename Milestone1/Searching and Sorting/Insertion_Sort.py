1e318ac021d9436b06cbe8aafef22cde9a76b8fb
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2023-06-25 01:16:06 +0530

    Create Insertion_Sort.py

---
+++ Milestone1/Searching and Sorting/Insertion_Sort.py
@@ -0,0 +1,7 @@
+# Provided with a random integer array/list(ARR) of size N, you have been required to sort this array using 'Insertion Sort'.
+# Sample Input 1:
+# 1
+# 7
+# 2 13 4 1 3 6 28
+# Sample Output 1:
+# 1 2 3 4 6 13 28

5812172671b0d112117b3cdb9ef2408fd80c27ee
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2023-07-01 10:31:11 +0530

    Create printKFreq.py

---
+++ Milestone1/Tuples_Dictionaries_Set/printKFreq.py
@@ -0,0 +1,11 @@
+def printKFreqWords(s,k):
+    words=s.split()
+    d={}
+    for w in words:
+        d[w]=d.get(w,0)+1
+    for w in d:
+        if d[w]==k:
+            print(w)
+
+s="this is a word string having many many word"
+printKFreqWords(s,1)

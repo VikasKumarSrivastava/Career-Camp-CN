39a3c64b0ff4586c59cd5af82daa3c3768a5bd91
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2023-07-08 10:46:21 +0530

    Create Power_of_a_number.py

---
+++ Milestone1/Recursion1/Power_of_a_number.py
@@ -0,0 +1,13 @@
+# Write a program to find x to the power n (i.e. x^n). Take x and n from the user. You need to print the answer.
+# Note : For this question, you can assume that 0 raised to the power of 0 is 1
+def power(x,n):
+    if n==0:
+        return 1
+    return x * power(x,n-1)
+
+x, n = input().split()
+x = int(x)
+n = int(n)
+ans = power(x,n)
+print(ans)
+

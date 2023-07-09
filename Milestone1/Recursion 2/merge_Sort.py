9edab2cb192fbdfa74c273b99ffabc390ab8e0a0
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2023-07-09 11:07:20 +0530

    Update merge_Sort.py

--- Milestone1/Recursion 2/merge_Sort.py
+++ Milestone1/Recursion 2/merge_Sort.py
@@ -1,3 +1,7 @@
+# Merge sort is a recursive algorithm that continuously splits the array in half until it cannot be further divided i.e., 
+# the array has only one element left (an array with one element is always sorted). 
+# Then the sorted subarrays are merged into one sorted array.
+# for more details visit: https://www.geeksforgeeks.org/merge-sort/
 def merge(arr1,arr2,arr):
     i=0
     j=0

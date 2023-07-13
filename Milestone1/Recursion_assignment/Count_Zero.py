26f78ced2be9570dc247bb53773d5ebf3a4cb765
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2023-07-13 23:17:25 +0530

    Update Count_Zero.py

--- Milestone1/Recursion_assignment/Count_Zero.py
+++ Milestone1/Recursion_assignment/Count_Zero.py
@@ -15,3 +15,28 @@
 # 2
 # Explanation for Sample Output 2 :
 # Even though "00010204" has 5 zeros, the output would still be 2 because when you convert it to an integer, it becomes 10204.
+from math import *
+from collections import *
+from sys import *
+from os import *
+
+## Read input as specified in the question.
+## Print output as specified in the question.
+
+
+def count_zero(n):
+    if n ==0:
+        return 0
+    rem = n%10
+    total = 0
+    if rem==0:
+        total +=1
+    total +=count_zero(n//10)
+    return total
+
+
+n = int(input())
+if n==0:
+    print(1)
+else:
+    print(count_zero(n))

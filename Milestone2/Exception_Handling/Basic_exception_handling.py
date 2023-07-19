b8a9aae4902976072012255da1f854aa19a0b1e6
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2023-07-19 23:57:21 +0530

    Create Basic_exception_handling.py

---
+++ Milestone2/Exception_Handling/Basic_exception_handling.py
@@ -0,0 +1,15 @@
+class Vehicle():
+    def __init__(self,make,model,fuel):
+        self.make=make
+        self.model=model
+        self.fuel=fuel
+    def get_value(self):
+        try:
+            age=2021-self.model
+            return 1000*(1/age)
+        except TypeError:
+            age=2021-int(self.model)
+            return 1000*(1/age)
+        except ZeroDivisionError:
+            age=2021-int(self.model)
+            return 1000*(1)

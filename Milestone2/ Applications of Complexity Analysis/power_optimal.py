b6459a6b5019a7b7d097c7f400d3943fd860feb2
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2023-07-27 00:12:36 +0530

    Update and rename power.py to power_optimal.py

--- Milestone2/ Applications of Complexity Analysis/power_optimal.py
+++ Milestone2/ Applications of Complexity Analysis/power_optimal.py
@@ -2,8 +2,11 @@ def power(x, n):
     # Please add your code here
     if n==0:
         return 1
-    return x * power(x,n-1)
-
+    small_output = power(x,n//2)
+    if n%2==0:
+        return small_output * small_output
+    else:
+        return x*small_output*small_output
 # Main
 from sys import setrecursionlimit
 setrecursionlimit(11000)

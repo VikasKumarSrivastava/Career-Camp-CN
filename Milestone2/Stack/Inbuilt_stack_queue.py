e080bf90b76c6b2b5951bf5e5f75e90161d3b2a2
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2023-08-06 10:49:22 +0530

    Create Inbuilt_stack_queue.py

---
+++ Milestone2/Stack/Inbuilt_stack_queue.py
@@ -0,0 +1,25 @@
+#inbuilt stack as list
+s=[1,2,3]
+s.append(4)
+s.append(5)
+print(s.pop())
+print(s.pop())
+
+#inbuilt queue
+import queue
+q=queue.Queue()
+q.put(1)
+q.put(2)
+q.put(3)
+q.put(4)
+while not q.empty():
+    print(q.get())
+  
+#queue as Stack
+import queue
+q=queue.LifoQueue()
+q.put(1)
+q.put(2)
+q.put(3)
+while not q.empty():
+    print(q.get())

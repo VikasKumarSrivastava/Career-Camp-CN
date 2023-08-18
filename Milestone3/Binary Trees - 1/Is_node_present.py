e2849a87c628d95ee502ecd5be3b7b183194d3ba
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2023-08-18 17:52:39 +0530

    Create Is_node_present.py

---
+++ Milestone3/Binary Trees - 1/Is_node_present.py
@@ -0,0 +1,85 @@
+# For a given Binary Tree of type integer and a number X, find whether a node exists in the tree with data X or not.
+# Sample Input 1:
+# 8 3 10 1 6 -1 14 -1 -1 4 7 13 -1 -1 -1 -1 -1 -1 -1
+# 7
+# Sample Output 1:
+# true
+# Explanation For Output 1:
+# Clearly, we can see that 7 is present in the tree. So, the output will be true.
+# Sample Input 2:
+# 2 3 4 -1 -1 -1 -1
+# 10
+# Sample Output 2:
+# false
+
+
+from sys import stdin, setrecursionlimit
+import queue
+
+setrecursionlimit(10 ** 6)
+#   Binary TreeNode class
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def isNodePresent(root, x):
+    # Write your code here.
+    if root == None:
+        return False
+    if root.data == x:
+        return True
+    ans1 = isNodePresent(root.left,x)
+    ans2 = isNodePresent(root.right,x)
+    return ans1 or ans2
+
+# To build the tree
+def buildLevelTree(levelorder):
+    index = 0
+    length = len(levelorder)
+
+    if length<=0 or levelorder[0]==-1:
+        return None
+
+    root = BinaryTreeNode(levelorder[index])
+    index += 1
+    q = queue.Queue()
+    q.put(root)
+
+    while not q.empty():
+
+        currentNode = q.get()
+        leftChild = levelorder[index]
+        index += 1
+
+        if leftChild != -1:
+
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left =leftNode
+            q.put(leftNode)
+
+        rightChild = levelorder[index]
+        index += 1
+
+        if rightChild != -1:
+
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right =rightNode
+            q.put(rightNode)
+
+    return root
+
+
+# Main
+levelOrder = [int(i) for i in input().strip().split()]
+root = buildLevelTree(levelOrder)
+
+x = int(input())
+
+present = isNodePresent(root, x)
+
+if present:
+    print('true')
+else:
+    print('false')

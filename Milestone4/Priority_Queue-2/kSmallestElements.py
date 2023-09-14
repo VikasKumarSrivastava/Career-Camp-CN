e0f6c9d090bf1d08241894fe5168554ee1603f98
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2023-09-14 21:45:55 +0530

    Update kSmallestElements.py

--- Milestone4/Priority_Queue-2/kSmallestElements.py
+++ Milestone4/Priority_Queue-2/kSmallestElements.py
@@ -1,1 +1,29 @@
-pass
+# You are given with an integer k and an array of integers that contain numbers in random order.
+# Write a program to find k smallest numbers from given array. You need to save them in an array and return it.
+# Time complexity should be O(n * logk) and space complexity should not be more than O(k).
+# Note: Order of elements in the output is not important.
+# Sample Input 1 :
+# 13
+# 2 12 9 16 10 5 3 20 25 11 1 8 6 
+# 4
+# Sample Output 1 :
+# 1 2 3 5 
+import heapq
+def kSmallest(lst, k):
+    ######################
+    #PLEASE ADD CODE HERE#
+    ######################
+    heapq.heapify(lst)
+    ans=[]
+    for i in range(0,k):
+       ans.append( heapq.heappop(lst))
+    return ans
+
+
+# Main
+n=int(input())
+lst=list(int(i) for i in input().strip().split(' '))
+k=int(input())
+ans=kSmallest(lst, k)
+ans.sort()
+print(*ans, sep=' ')

4d3e1f03af32d94e1339c0a07f7a5d180a470aa9
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2023-09-18 19:56:45 +0530

    Update minStepTo1.py

--- Milestone4/DP1/minStepTo1.py
+++ Milestone4/DP1/minStepTo1.py
@@ -22,7 +22,7 @@
 # Step 3 : n = 2 / 2 = 1  
 from sys import stdin
 from sys import maxsize as MAX_VALUE
-
+#tabulation method/iterative/Bottom Up approach
 def countMinStepsToOne(n) :
     dp=[0]*(n+1)
     dp[1]=0
@@ -38,6 +38,37 @@ def countMinStepsToOne(n) :
 
         dp[i]= min(op1,op2,op3)+1
     return dp[n]
+#memoization - top down approach
+import sys
+def minStepsTo1(n,dp):
+    if n==1:
+        return 0
+    ans1=sys.maxsize
+    if n% 3==0:
+        if dp[n//3]==-1:
+            ans1=minStepsTo1(n//3,dp)
+            dp[n//3]=ans1
+        else:
+            ans1=dp[n//3]
+    ans2=sys.maxsize
+    
+    if n%2==0:
+        if dp[n//2]==-1:
+            ans2=minStepsTo1(n//2,dp)
+            dp[n//2]=ans2
+        else:
+            ans2=dp[n//2]
+    if dp[n-1]==-1:
+        ans3=minStepsTo1(n-1,dp)
+        dp[n-1]=ans3
+    else:
+        ans3=dp[n-1]   
+    ans=1+min(ans1,ans2,ans3)
+    return ans
+n=int(input())
+dp=[-1 for i in range(n+1)]
+ans=minStepsTo1(n,dp)
+print(ans)
 
 
 #main

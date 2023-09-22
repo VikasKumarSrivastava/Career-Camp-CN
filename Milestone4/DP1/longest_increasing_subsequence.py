2559d3b330bc810bcb1d945516c642f3a34e6dff
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2023-09-22 19:55:38 +0530

    Update longest_increasing_subsequence.py

--- Milestone4/DP1/longest_increasing_subsequence.py
+++ Milestone4/DP1/longest_increasing_subsequence.py
@@ -29,3 +29,34 @@
 # li=[int(ele) for ele in input().split()]
 # ans=lis(li,0,n)[1]
 # print(ans)
+from sys import stdin
+
+#TC O(N^2)
+#SC O(N)
+def lis(arr): 
+    # Write your code here
+    dp =[None for i in range(n)]
+    dp[0]=1
+    ans=1
+    for i in range(1,n):
+        max =1
+        for j in range(i-1,-1,-1):
+            if arr[j]<arr[i]:
+                op=dp[j]+1
+                if op>max:
+                    max =op
+        dp[i] = max
+        if (max>ans):
+            ans= max
+    return ans
+def takeInput():
+    #To take fast I/O
+    n=int(stdin.readline().strip())
+    if n==0:
+        return list(),0
+    arr=list(map(int,stdin.readline().strip().split( )))
+    return arr,n
+    
+
+arr,n=takeInput()
+print(lis(arr))
